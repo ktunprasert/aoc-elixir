33c2ae54c62d6c710795eec203d1a4619ac44d45
ktunprasert <ktunprasert@outlook.com>
2023-11-14 18:43:57 +0000

    2020.day1: part 2 added

--- lib/2020/day1.ex
+++ lib/2020/day1.ex
@@ -1,24 +1,42 @@
 defmodule Aoc.Y2020.D1 do
-  use Aoc.Runner
-import Aoc.Util
+  use Aoc.Runner, inspect: true
+
+  import Aoc.Util
 
   def part1(input) do
     numbers = helper(input)
 
     Enum.reduce_while(
-      numbers,
+      for i <- numbers, j <- numbers do
+        [i, j]
+      end,
       0,
-      fn i, acc ->
-        case Enum.find(numbers, &(&1 == 2020 - i)) do
-          nil -> {:cont, acc}
-          j -> {:halt, i * j}
-        end
+      fn
+        [i, j], _acc when i + j == 2020 ->
+          {:halt, i * j}
+
+        _, acc ->
+          {:cont, acc}
       end
     )
   end
 
   def part2(input) do
-    :ok
+    numbers = helper(input)
+
+    Enum.reduce_while(
+      for i <- numbers, j <- numbers, k <- numbers do
+        [i, j, k]
+      end,
+      0,
+      fn
+        [i, j, k], _acc when i + j + k == 2020 ->
+          {:halt, i * j * k}
+
+        _, acc ->
+          {:cont, acc}
+      end
+    )
   end
 
   def helper(input) do
