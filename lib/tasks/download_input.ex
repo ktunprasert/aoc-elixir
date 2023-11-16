dfde56b9bbb0312c4ec1e521771644ae63d4dfa5
ktunprasert <ktunprasert@outlook.com>
2023-11-16 16:40:52 +0000

    task: refactored to client

--- lib/tasks/download_input.ex
+++ lib/tasks/download_input.ex
@@ -7,17 +7,9 @@ defmodule Mix.Tasks.Download do
 
     Mix.Task.run("app.start")
 
-    cookie = Dotenv.get("AOC_SESSION")
-
-    res = Req.get!("https://adventofcode.com/#{year}/day/#{day}/input",
-      headers: [
-        {"Cookie", "session=#{cookie}"}
-      ]
-    )
-
     filename = "priv/inputs/#{year}/day#{day}.txt"
 
-    File.write(filename, res.body)
+    File.write(filename, Aoc.Client.get_input(year, day))
 
     IO.puts("Downloaded input for day #{day} of year #{year} to #{filename}")
   end
