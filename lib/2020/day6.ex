1cae9885ef56270e2b778b4e318685f8585a6c2c
ktunprasert <ktunprasert@outlook.com>
2023-11-17 21:30:46 +0000

    2020.6: part 1 - reduce right away

--- lib/2020/day6.ex
+++ lib/2020/day6.ex
@@ -5,14 +5,16 @@ defmodule Aoc.Y2020.D6 do
 
   def part1(input) do
     helper(input)
-    |> Enum.map(fn group ->
-      group
-      |> String.graphemes()
-      |> Enum.filter(&(&1 != "\n"))
-      |> Enum.uniq()
-      |> Enum.count()
+    |> Enum.reduce(0, fn group, acc ->
+      unique =
+        group
+        |> String.graphemes()
+        |> Enum.filter(&(&1 != "\n"))
+        |> Enum.uniq()
+        |> Enum.count()
+
+      unique + acc
     end)
-    |> Enum.reduce(&+/2)
   end
 
   def part2(input) do
