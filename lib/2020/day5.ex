a295c300fa25e17e103ee911529f9690c05a582a
ktunprasert <ktunprasert@outlook.com>
2023-11-17 12:36:29 +0000

    2020.5: init and helper defined

---
+++ lib/2020/day5.ex
@@ -0,0 +1,40 @@
+defmodule Aoc.Y2020.D5 do
+  use Aoc.Runner, inspect: true
+
+  import Aoc.Util
+
+  @fb_pair {0, 127}
+  @lr_pair {0, 7}
+  def part1(input) do
+    :ok
+  end
+
+  def part2(input) do
+    :ok
+  end
+
+  def decode(code_str) do
+    [fb, lr] = code_str |> String.graphemes() |> Enum.chunk_every(7)
+
+    row = decode(fb, @fb_pair)
+    col = decode(lr, @lr_pair)
+
+    row * 8 + col
+  end
+
+  def decode([c | rest], {min, max} = pair) when c in ["F", "L"] do
+    decode(rest, {min, max - distance(pair)})
+  end
+
+  def decode([c | rest], {min, max} = pair) when c in ["B", "R"] do
+    decode(rest, {min + distance(pair), max})
+  end
+
+  def decode(_, {same, same}), do: same
+
+  defp distance({min, max}), do: div(max + 1 - min, 2)
+
+  def helper(input) do
+    input |> parse_lines()
+  end
+end
---
+++ test/2020/day5_test.exs
@@ -0,0 +1,39 @@
+defmodule AocTest.Y2020.D5 do
+  use ExUnit.Case, async: true
+
+  alias Aoc.Y2020.D5, as: Solver
+
+  @example ""
+  @fb_pair {0, 127}
+  @lr_pair {0, 7}
+
+  describe "Example case" do
+    test "part 1" do
+      assert Solver.part1(@example) == :ok
+    end
+
+    test "part 2" do
+      assert Solver.part2(@example) == :ok
+    end
+  end
+
+  describe "Example helpers" do
+    test "decode FB" do
+      assert Solver.decode("FBFBBFF" |> String.graphemes(), @fb_pair) == 44
+      assert Solver.decode("BFFFBBF" |> String.graphemes(), @fb_pair) == 70
+      assert Solver.decode("FFFBBBF" |> String.graphemes(), @fb_pair) == 14
+      assert Solver.decode("BBFFBBF" |> String.graphemes(), @fb_pair) == 102
+    end
+
+    test "decode LR" do
+      assert Solver.decode("RRR" |> String.graphemes(), @lr_pair) == 7
+      assert Solver.decode("RLL" |> String.graphemes(), @lr_pair) == 4
+    end
+
+    test "decode full" do
+      assert Solver.decode("BFFFBBFRRR") == 567
+      assert Solver.decode("FFFBBBFRRR") == 119
+      assert Solver.decode("BBFFBBFRLL") == 820
+    end
+  end
+end
