7af8f9c84ddffd22a02b36388f7d2fb931b93f48
ktunprasert <ktunprasert@outlook.com>
2023-11-25 12:39:02 +0000

    2020.12: part 2 use operators

--- lib/2020/day12.ex
+++ lib/2020/day12.ex
@@ -31,65 +31,53 @@ defmodule Aoc.Y2020.D12 do
     |> manhattan()
   end
 
-  def manhattan({{n, e, s, w}, _}) do
-    abs(n - s) + abs(e - w)
-  end
+  def manhattan({{n, e, s, w}, _}), do: abs(n - s) + abs(e - w)
 
   def move_with_waypoint({code, unit}, pos, waypoint) do
     case code do
-      "F" -> {traverse(pos, unit, waypoint), waypoint}
-      "N" -> {pos, pos_move(waypoint, unit, :north)}
-      "E" -> {pos, pos_move(waypoint, unit, :east)}
-      "S" -> {pos, pos_move(waypoint, unit, :south)}
-      "W" -> {pos, pos_move(waypoint, unit, :west)}
+      "F" -> {pos +++ (waypoint ~> unit), waypoint}
+      "N" -> {pos, waypoint +++ {unit, 0, 0, 0}}
+      "E" -> {pos, waypoint +++ {0, unit, 0, 0}}
+      "S" -> {pos, waypoint +++ {0, 0, unit, 0}}
+      "W" -> {pos, waypoint +++ {0, 0, 0, unit}}
       "R" -> {pos, rotate(waypoint, unit, true)}
       "L" -> {pos, rotate(waypoint, unit, false)}
     end
   end
 
-  def traverse({n, e, s, w}, factor, {wn, we, ws, ww}) do
-    {n + wn * factor, e + we * factor, s + ws * factor, w + ww * factor}
-  end
-
-  def rotate({n, e, s, w} = pos, rotation, clockwise) do
+  def rotate({n, e, s, w}, rotation, clockwise) do
     rotation = if clockwise, do: rotation, else: 360 - rotation
 
     case rotation do
       90 -> {w, n, e, s}
       180 -> {s, w, n, e}
       270 -> {e, s, w, n}
-      _ -> pos
     end
   end
 
-  def move({code, unit}, {n, e, s, w} = pos, facing) do
+  def move({code, unit}, pos, facing) do
     direction =
       case facing do
-        0 -> :north
-        90 -> :east
-        180 -> :south
-        270 -> :west
+        0 -> {unit, 0, 0, 0}
+        90 -> {0, unit, 0, 0}
+        180 -> {0, 0, unit, 0}
+        270 -> {0, 0, 0, unit}
       end
 
     case code do
-      "F" -> {pos_move(pos, unit, direction), facing}
-      "N" -> {pos_move(pos, unit, :north), facing}
-      "E" -> {pos_move(pos, unit, :east), facing}
-      "S" -> {pos_move(pos, unit, :south), facing}
-      "W" -> {pos_move(pos, unit, :west), facing}
-      "R" -> {{n, e, s, w}, rem(facing + unit, 360)}
-      "L" -> {{n, e, s, w}, rem(facing - unit, 360)}
+      "F" -> {pos +++ direction, facing}
+      "N" -> {pos +++ {unit, 0, 0, 0}, facing}
+      "E" -> {pos +++ {0, unit, 0, 0}, facing}
+      "S" -> {pos +++ {0, 0, unit, 0}, facing}
+      "W" -> {pos +++ {0, 0, 0, unit}, facing}
+      "R" -> {pos, rem(facing + unit + 360, 360)}
+      "L" -> {pos, rem(facing - unit + 360, 360)}
     end
-    |> then(fn
-      {pos, facing} when facing < 0 -> {pos, facing + 360}
-      self -> self
-    end)
   end
 
-  def pos_move({n, e, s, w}, unit, :north), do: {n + unit, e, s, w}
-  def pos_move({n, e, s, w}, unit, :east), do: {n, e + unit, s, w}
-  def pos_move({n, e, s, w}, unit, :south), do: {n, e, s + unit, w}
-  def pos_move({n, e, s, w}, unit, :west), do: {n, e, s, w + unit}
+  def {a, b, c, d} +++ {w, x, y, z}, do: {a + w, b + x, c + y, d + z}
+
+  def {a, b, c, d} ~> factor, do: {a * factor, b * factor, c * factor, d * factor}
 
   def helper(input) do
     input
