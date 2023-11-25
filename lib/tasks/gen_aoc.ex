be0acde94269c62753e4ab78f8d710e8de9d2608
ktunprasert <ktunprasert@outlook.com>
2023-11-25 15:55:14 +0000

    task.gen_aoc: remove fluff

--- lib/tasks/gen_aoc.ex
+++ lib/tasks/gen_aoc.ex
@@ -13,17 +13,8 @@ defmodule Mix.Tasks.Gen.Aoc do
     Mix.Generator.create_directory("lib/#{year}")
     Mix.Generator.copy_template("priv/templates/aoc.ex.eex", "lib/#{year}/day#{day}.ex", day: day, year: year)
     Mix.Generator.copy_template("priv/templates/aoc_test.ex.eex", "test/#{year}/day#{day}_test.exs", day: day, year: year)
-    Mix.Generator.create_file("priv/inputs/#{year}/day#{day}.txt", generate_input(year, day))
+    Mix.Generator.create_file("priv/inputs/#{year}/day#{day}.txt", Aoc.Client.get_input(year, day))
 
     IO.puts("Done!")
-  end
-
-  defp generate_input(year, day) do
-    Aoc.Client.get_input(year, day)
-  end
-
-  defp write_file(content, path) do
-    File.write!(path, content)
-    IO.puts("Created file at #{path}")
   end
 end
