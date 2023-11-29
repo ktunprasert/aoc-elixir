d9b49664370a4d60c0e732b15865e359f10b73ad
ktunprasert <ktunprasert@outlook.com>
2023-11-29 15:15:52 +0000

    2020.14: part 2

--- lib/2020/day14.ex
+++ lib/2020/day14.ex
@@ -12,8 +12,9 @@ defmodule Aoc.Y2020.D14 do
         {:mask, mask} ->
           {map, mask}
 
-        {:mem, addr, bitstring} ->
-          {Map.put(map, addr, mask(mask, String.pad_leading(bitstring, 36, "0"))), mask}
+        {:mem, addr, dec} ->
+          bitstring = dec |> Integer.to_string(2)
+          {Map.put(map, addr, mask(mask, bitstring)), mask}
       end
     end)
     |> elem(0)
@@ -25,7 +26,51 @@ defmodule Aoc.Y2020.D14 do
   end
 
   def part2(input) do
-    :ok
+    lines = helper(input)
+
+    lines
+    |> Enum.reduce({%{}, nil}, fn line, {map, mask} ->
+      case parse(line) do
+        {:mask, mask} ->
+          {map, mask}
+
+        {:mem, addr, dec} ->
+          addr_bitstring = addr |> Integer.to_string(2)
+          {addr_mask, num_x} = mask_v2(mask, addr_bitstring)
+
+          map =
+            0..(2 ** num_x - 1)
+            |> Enum.map(fn n ->
+              mask_v3(
+                addr_mask |> List.to_string(),
+                Integer.to_string(n, 2) |> String.pad_leading(num_x, "0")
+              )
+              |> List.to_string()
+              |> Integer.parse(2)
+            end)
+            |> Enum.reduce(map, fn {addr, _}, map ->
+              Map.put(map, addr, dec)
+            end)
+
+          {map, mask}
+      end
+    end)
+    |> elem(0)
+    |> Enum.reduce(0, fn {_key, val}, acc -> val + acc end)
+  end
+
+  def mask_v3(mask_x, num) do
+    mask_v3(mask_x, num, [])
+  end
+
+  def mask_v3(<<>>, <<>>, acc), do: acc |> Enum.reverse()
+  def mask_v3(<<a, a_rest::binary>>, <<>>, acc), do: mask_v3(a_rest, <<>>, [a | acc])
+
+  def mask_v3(<<a, a_rest::binary>>, <<b, b_rest::binary>> = num, acc) do
+    case {a, b} do
+      {?X, _} -> mask_v3(a_rest, b_rest, [b | acc])
+      _ -> mask_v3(a_rest, num, [a | acc])
+    end
   end
 
   def parse("mask = " <> mask) do
@@ -34,11 +79,26 @@ defmodule Aoc.Y2020.D14 do
 
   def parse("mem" <> mem) do
     [_, addr, dec] = Regex.run(~r/\[(\d+)\] = (\d+)/, mem)
-    {:mem, addr, dec |> String.to_integer() |> Integer.to_string(2)}
+    {:mem, addr |> String.to_integer(), dec |> String.to_integer()}
+  end
+
+  def mask_v2(mask, num) do
+    mask_v2(mask, num |> String.pad_leading(36, "0"), [], 0)
+  end
+
+  def mask_v2(_, <<>>, acc, num_x), do: {acc |> Enum.reverse(), num_x}
+
+  def mask_v2(<<a, a_rest::binary>>, <<b, b_rest::binary>>, acc, nx) do
+    case {a, b} do
+      {?1, _} -> mask_v2(a_rest, b_rest, [?1 | acc], nx)
+      {?0, _} -> mask_v2(a_rest, b_rest, [b | acc], nx)
+      {?X, _} -> mask_v2(a_rest, b_rest, [?X | acc], nx + 1)
+      _ -> mask_v2(a_rest, b_rest, [b | acc], nx)
+    end
   end
 
   def mask(mask, num) do
-    mask(mask, num, [])
+    mask(mask, num |> String.pad_leading(36, "0"), [])
   end
 
   def mask(_, <<>>, acc), do: acc |> Enum.reverse()
--- test/2020/day14_test.exs
+++ test/2020/day14_test.exs
@@ -10,25 +10,32 @@ defmodule AocTest.Y2020.D14 do
   mem[8] = 0
   """
 
+  @example_v2 """
+  mask = 000000000000000000000000000000X1001X
+  mem[42] = 100
+  mask = 00000000000000000000000000000000X0XX
+  mem[26] = 1
+  """
+
   describe "Example case" do
     test "part 1" do
       assert Solver.part1(@example) == 165
     end
 
     test "part 2" do
-      assert Solver.part2(@example) == :ok
+      assert Solver.part2(@example_v2) == 208
     end
   end
 
   describe "Helpers" do
     test "parse_mem/1" do
-      assert Solver.parse("mem[8] = 11") == {:mem, "8", "1011"}
+      assert Solver.parse("mem[8] = 11") == {:mem, 8, 11}
       assert Solver.parse("mask = XX0") == {:mask, "XX0"}
     end
 
     test "mask/2" do
-      assert Solver.mask("0000", "1101") == ~c"0000"
-      assert Solver.mask("1111", "0000") == ~c"1111"
+      assert Solver.mask("0000", "1101", []) == ~c"0000"
+      assert Solver.mask("1111", "0000", []) == ~c"1111"
     end
   end
 end
