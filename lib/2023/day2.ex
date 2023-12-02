bbf72a2d54b4e43bddab2ef39e01dc37b5dafd38
ktunprasert <ktunprasert@outlook.com>
2023-12-02 12:18:11 +0000

    2023.2: part 1

---
+++ lib/2023/day2.ex
@@ -0,0 +1,57 @@
+defmodule Aoc.Y2023.D2 do
+  use Aoc.Runner, inspect: true
+
+  import Aoc.Util
+
+  @cubes {12, 13, 14}
+  def part1(input) do
+    lines = input |> parse_lines()
+
+    lines
+    |> Enum.reduce({1, 0}, fn line, {n, acc} ->
+      [_, gamestr] = String.split(line, ": ")
+
+      state =
+        gamestr
+        |> String.split(";\s")
+        |> Enum.reduce_while(:end, fn str, _ ->
+          play =
+            String.split(str, ",\s")
+            |> Enum.reduce({0, 0, 0}, fn str, cube ->
+              play_cube(cube, str)
+            end)
+
+          cond do
+            play >>> @cubes -> {:halt, :end}
+            true -> {:cont, :found}
+          end
+        end)
+
+      case state do
+        :end -> {n + 1, acc}
+        _ -> {n + 1, acc + n}
+      end
+    end)
+    |> elem(1)
+  end
+
+  def part2(input) do
+    :ok
+  end
+
+  def play_cube({r, g, b}, str) do
+    case Integer.parse(str) do
+      {n, " red"} -> {r + n, g, b}
+      {n, " green"} -> {r, g + n, b}
+      {n, " blue"} -> {r, g, b + n}
+    end
+  end
+
+  def {a, b, c} >>> {x, y, z} do
+    a > x || b > y || c > z
+  end
+
+  def helper(input) do
+    input |> parse_lines()
+  end
+end
