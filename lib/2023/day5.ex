9818f3c1f4c369e4adfab070833deb35fcc92ee4
ktunprasert <ktunprasert@outlook.com>
2023-12-05 13:49:44 +0000

    2023.5: optimised but still expensive

--- lib/2023/day5.ex
+++ lib/2023/day5.ex
@@ -66,27 +66,24 @@ defmodule Aoc.Y2023.D5 do
       |> Enum.map(&String.to_integer/1)
       |> Enum.chunk_every(3)
       |> Enum.map(fn [a, b, c] ->
-        fn i ->
-          if i in b..(b + c - 1) do
-            a + (i - b)
-          else
-            nil
-          end
-        end
+        {a, b..(b + c - 1), b}
       end)
+      |> Enum.sort()
     end)
-    |> Enum.reduce(seeds, fn pipe_fns, seeds ->
+    |> Enum.reduce(seeds, fn pipe_tuples, seeds ->
       seeds
       |> Enum.map(fn seed ->
-        Enum.reduce_while(pipe_fns, seed, fn f, acc ->
-          case f.(seed) do
-            nil -> {:cont, acc}
-            found -> {:halt, found}
+        Enum.reduce_while(pipe_tuples, seed, fn {a, range, b}, seed ->
+          if seed in range do
+            {:halt, a + (seed - b)}
+          else
+            {:cont, seed}
           end
         end)
       end)
     end)
     |> Enum.min()
+
   end
 
   def helper(input) do
