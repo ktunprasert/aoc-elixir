b268815a211e87e4ae305bcd54f82a3c7ef5a717
ktunprasert <ktunprasert@outlook.com>
2023-12-06 22:48:12 +0000

    2023.6: part 2 washed again

--- lib/2023/day6.ex
+++ lib/2023/day6.ex
@@ -6,7 +6,7 @@ defmodule Aoc.Y2023.D6 do
   def part1(input) do
     input
     |> helper
-    |> Enum.map(&Enum.map(&1, fn i -> String.to_integer(i) end))
+    |> Enum.map(fn i -> Enum.map(i, &String.to_integer/1) end)
     |> Enum.zip()
     |> Enum.reduce(1, fn {t, d}, acc -> acc * win_quadratic(t, d) end)
   end
@@ -22,9 +22,9 @@ defmodule Aoc.Y2023.D6 do
   def part2(input) do
     input
     |> helper
-    |> Enum.map(&(Enum.join(&1) |> String.to_integer()))
-    |> List.to_tuple()
-    |> then(fn {t, d} -> win_quadratic(t, d) end)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.map(&String.to_integer/1)
+    |> then(fn [t, d] -> win_quadratic(t, d) end)
   end
 
   def helper(input) do
