a201e4b55723b001401c9b0610fb77f42710487f
ktunprasert <ktunprasert@outlook.com>
2023-12-06 22:45:25 +0000

    2023.6: part 2 washed

--- lib/2023/day6.ex
+++ lib/2023/day6.ex
@@ -4,12 +4,10 @@ defmodule Aoc.Y2023.D6 do
   import Aoc.Util
 
   def part1(input) do
-    races =
-      helper(input)
-      |> Enum.map(&Enum.map(&1, fn i -> String.to_integer(i) end))
-      |> Enum.zip()
-
-    races
+    input
+    |> helper
+    |> Enum.map(&Enum.map(&1, fn i -> String.to_integer(i) end))
+    |> Enum.zip()
     |> Enum.reduce(1, fn {t, d}, acc -> acc * win_quadratic(t, d) end)
   end
 
@@ -22,22 +20,16 @@ defmodule Aoc.Y2023.D6 do
   end
 
   def part2(input) do
-    {time, dist} =
-      helper(input)
-      |> Enum.map(&(Enum.join(&1) |> String.to_integer()))
-      |> List.to_tuple()
-
-    win_quadratic(time, dist)
+    input
+    |> helper
+    |> Enum.map(&(Enum.join(&1) |> String.to_integer()))
+    |> List.to_tuple()
+    |> then(fn {t, d} -> win_quadratic(t, d) end)
   end
 
   def helper(input) do
     input
     |> parse_lines()
-    |> Enum.map(fn
-      line ->
-        line
-        |> String.split()
-        |> tl()
-    end)
+    |> Enum.map(fn line -> line |> String.split() |> tl() end)
   end
 end
