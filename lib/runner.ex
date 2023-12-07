6bb7bb0b5d44d01c1957830c66e793d4813ce35d
ktunprasert <ktunprasert@outlook.com>
2023-12-07 17:02:40 +0000

    runner: washed

--- lib/runner.ex
+++ lib/runner.ex
@@ -1,64 +1,45 @@
 defmodule Aoc.Runner do
   def run(year, day, part \\ nil) do
     module = "Elixir.Aoc.Y#{year}.D#{day}" |> String.to_existing_atom()
-
-    if part do
-      apply(module, :run, [part])
-    else
-      apply(module, :run, [])
-    end
+    args = if part, do: [part], else: []
+    apply(module, :run, args)
   end
 
   defmacro __using__(opts) do
-    inspect? = Keyword.get(opts, :inspect, false)
+    default = [inspect: false, input: true]
+    opts = Keyword.merge(default, opts)
 
     quote do
       def run() do
-        part1 = run_part1()
-        part2 = run_part2()
-
-        if unquote(inspect?) do
-          part1 |> IO.inspect(label: "Part 1")
-          part2 |> IO.inspect(label: "Part 2")
-        end
+        [1, 2]
+        |> Enum.each(fn part ->
+          case unquote(opts[:inspect]) do
+            true -> run(part) |> IO.inspect(label: "Part #{part}")
+            false -> run(part)
+          end
+        end)
 
         :ok
       end
 
-      def run(1), do: run_part1()
-      def run(2), do: run_part2()
-
-      def run_part1() do
-        part1(get_input())
-      end
-
-      def run_part2() do
-        part2(get_input())
-      end
-
-      defp get_input() do
-        module_name = to_string(__MODULE__)
-        [year, day] = parse_module_name(module_name)
-        input_path = "/priv/inputs/#{year}/day#{day}.txt"
-
-        if !File.exists?(File.cwd!() <> input_path) do
-          nil
-        else
-          File.read!(File.cwd!() <> input_path)
-        end
-      end
+      def run(1), do: get_input() |> part1()
+      def run(2), do: get_input() |> part2()
 
       def parse_module_name(module_name) do
         module_name
         |> String.split(".")
         |> Enum.take(-2)
-        |> Enum.map(&grab_digits/1)
+        |> Enum.map(fn <<_, num::binary>> -> num |> String.to_integer() end)
       end
 
-      def grab_digits(string) do
-        case Regex.run(~r/\d+/, string) do
-          lst when is_list(lst) -> lst |> List.first()
-          nil -> nil
+      if unquote(opts[:input]) do
+        def get_input() do
+          module_name = to_string(__MODULE__)
+          [year, day] = parse_module_name(module_name)
+          input_path = "/priv/inputs/#{year}/day#{day}.txt"
+
+          # Let it explode for file not found, better than nil
+          File.read!(File.cwd!() <> input_path)
         end
       end
     end
--- test/runner_test.exs
+++ test/runner_test.exs
@@ -1,8 +1,10 @@
-defmodule Aoc.Y2021.D25 do
-  use Aoc.Runner
+defmodule Aoc.Y9999.D25 do
+  use Aoc.Runner, input: false
 
-  def part1(_input_path), do: :ok_part1
-  def part2(_input_path), do: :ok_part2
+  def part1(_input_path), do: :ok
+  def part2(_input_path), do: :ok
+
+  def get_input(), do: nil
 end
 
 defmodule Aoc.RunnerTest do
@@ -11,31 +13,20 @@ defmodule Aoc.RunnerTest do
   # Mock module to use Aoc.Runner
   describe "Aoc.Runner __using__ macro" do
     test "injects run/0 function into the module" do
-      assert Aoc.Y2021.D25.run() == :ok
+      assert Aoc.Y9999.D25.run() == :ok
     end
   end
 
-  describe "run_part1/0" do
-    test "calls part1/1 with input" do
-      assert Aoc.Y2021.D25.run_part1() == :ok_part1
-    end
-  end
-
-  describe "run_part2/0" do
-    test "calls part2/1 with input" do
-      assert Aoc.Y2021.D25.run_part2() == :ok_part2
+  describe "able to run both parts" do
+    test "run(1) and run(2)" do
+      assert Aoc.Y9999.D25.run(1) == :ok
+      assert Aoc.Y9999.D25.run(2) == :ok
     end
   end
 
   describe "parse_module_name/1" do
     test "parses module name correctly" do
-      assert Aoc.Y2021.D25.parse_module_name("Aoc.Y2021.D25") == ["2021", "25"]
-    end
-  end
-
-  describe "grab_digits/1" do
-    test "extracts digits from string" do
-      assert Aoc.Y2021.D25.grab_digits("D25") == "25"
+      assert Aoc.Y9999.D25.parse_module_name("Aoc.Y9999.D25") == [9999, 25]
     end
   end
 end
