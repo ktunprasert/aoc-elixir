3e29fc164494d8fee7782f1f769c75b1e95a5f89
ktunprasert <ktunprasert@outlook.com>
2023-12-10 17:11:31 +0000

    2023.10: unwashed

--- lib/2023/day10.ex
+++ lib/2023/day10.ex
@@ -12,64 +12,84 @@ defmodule Aoc.Y2023.D10 do
   def part1(input) do
     traverse_map = :ets.new(:traverse_map, [:duplicate_bag])
     table = helper(input)
-    start = :ets.lookup_element(table, :start, 2)
 
-    {[start], 0}
+    :ets.lookup_element(table, :start, 2)
+    |> orthogonal_dirs()
+    |> Stream.flat_map(fn {pos, from} ->
+      case :ets.lookup(table, pos) do
+        [{_, sym}] -> [{pos, sym, from}]
+        _ -> []
+      end
+    end)
+    |> Stream.flat_map(&traverse_rel(&1, table, traverse_map, 0))
+    |> then(&{&1, 1})
     |> Stream.iterate(fn {pos_list, dist} ->
       pos_list
-      |> Enum.flat_map(&traverse(&1, table, traverse_map, dist))
+      |> Enum.flat_map(&traverse_rel(&1, table, traverse_map, dist))
       |> then(&{&1, dist + 1})
     end)
-    |> Enum.take_while(fn {pos_list, _dist} -> pos_list !== [] end)
-    |> Enum.max_by(fn {_pos_list, dist} -> dist end)
-    |> then(fn {_pos_list, dist} -> dist + 1 end)
-    |> dbg
+    |> Enum.reduce_while(nil, fn
+      {[], dist}, _ -> {:halt, dist}
+      _, _ -> {:cont, nil}
+    end)
   end
 
   def part2(input) do
     :ok
   end
 
-  def traverse(pos, table, traverse_map, prev) do
-    pos
-    |> orthogonal_dirs()
-    |> Enum.flat_map(fn {pos, dir} ->
-      case :ets.lookup(table, pos) do
-        [] ->
-          []
+  def traverse_rel({pos, sym, from_dir}, table, traverse_map, prev),
+    do: traverse_rel(pos, sym, from_dir, table, traverse_map, prev)
 
-        [{_, char}] ->
-          case {char, dir} do
-            {char, :n} when char in @from_south -> [pos]
-            {char, :s} when char in @from_north -> [pos]
-            {char, :e} when char in @from_west -> [pos]
-            {char, :w} when char in @from_east -> [pos]
-            _ -> []
-          end
-      end
-    end)
-    |> Enum.flat_map(fn pos ->
-      case :ets.lookup(traverse_map, pos) do
-        [] ->
-          :ets.insert(traverse_map, {pos, prev + 1})
-          [pos]
+  def traverse_rel(pos, sym, from_dir, table, traverse_map, prev) do
+    case pos |> move_rel(sym, from_dir) do
+      nil ->
+        []
 
-        [{_, found}] when prev + 1 <= found ->
-          :ets.insert(traverse_map, {pos, prev + 1})
-          [pos]
+      {new_pos, dir} ->
+        sym = :ets.lookup_element(table, new_pos, 2)
 
-        _ ->
-          []
-      end
-    end)
+        case :ets.lookup(traverse_map, new_pos) do
+          [] ->
+            :ets.insert(traverse_map, {new_pos, prev + 1})
+            [{new_pos, sym, dir}]
+
+          [{_, found}] when prev + 1 < found ->
+            :ets.insert(traverse_map, {new_pos, prev + 1})
+            [{new_pos, sym, dir}]
+
+          _ ->
+            []
+        end
+    end
   end
+
+  def move_rel(pos, sym, from_dir) do
+    case {sym, from_dir} do
+      {?|, :s} -> {pos +++ {-1, 0}, :s}
+      {?|, :n} -> {pos +++ {1, 0}, :n}
+      {?-, :w} -> {pos +++ {0, 1}, :w}
+      {?-, :e} -> {pos +++ {0, -1}, :e}
+      {?L, :n} -> {pos +++ {0, 1}, :w}
+      {?L, :e} -> {pos +++ {-1, 0}, :s}
+      {?J, :n} -> {pos +++ {0, -1}, :e}
+      {?J, :w} -> {pos +++ {-1, 0}, :s}
+      {?7, :s} -> {pos +++ {0, -1}, :e}
+      {?7, :w} -> {pos +++ {1, 0}, :n}
+      {?F, :s} -> {pos +++ {0, 1}, :w}
+      {?F, :e} -> {pos +++ {1, 0}, :n}
+      _ -> nil
+    end
+  end
+
+  def {a, b} +++ {x, y}, do: {a + x, b + y}
 
   def orthogonal_dirs({x, y}) do
     [
-      {{x + 1, y}, :n},
       {{x - 1, y}, :s},
-      {{x, y + 1}, :e},
-      {{x, y - 1}, :w}
+      {{x + 1, y}, :n},
+      {{x, y + 1}, :w},
+      {{x, y - 1}, :e}
     ]
   end
 
--- test/2023/day10_test.exs
+++ test/2023/day10_test.exs
@@ -29,4 +29,21 @@ defmodule AocTest.Y2023.D10 do
       assert Solver.part2(@example) == :ok
     end
   end
+
+  describe "Helper" do
+    test "move_rel/3" do
+      assert Solver.move_rel({0, 0}, ?|, :s) == {{-1, 0}, :s}
+      assert Solver.move_rel({0, 0}, ?|, :n) == {{1, 0}, :n}
+      assert Solver.move_rel({0, 0}, ?-, :w) == {{0, 1}, :w}
+      assert Solver.move_rel({0, 0}, ?-, :e) == {{0, -1}, :e}
+      assert Solver.move_rel({0, 0}, ?L, :n) == {{0, 1}, :w}
+      assert Solver.move_rel({0, 0}, ?L, :e) == {{-1, 0}, :s}
+      assert Solver.move_rel({0, 0}, ?J, :n) == {{0, -1}, :e}
+      assert Solver.move_rel({0, 0}, ?J, :w) == {{-1, 0}, :s}
+      assert Solver.move_rel({0, 0}, ?7, :s) == {{0, -1}, :e}
+      assert Solver.move_rel({0, 0}, ?7, :w) == {{1, 0}, :n}
+      assert Solver.move_rel({0, 0}, ?F, :s) == {{0, 1}, :w}
+      assert Solver.move_rel({0, 0}, ?F, :e) == {{1, 0}, :n}
+    end
+  end
 end
