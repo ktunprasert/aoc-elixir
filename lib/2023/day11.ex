31a62b492eda1d04a7b60decff379ee79dcbcf85
ktunprasert <ktunprasert@outlook.com>
2023-12-11 14:16:02 +0000

    2023.11: as wash as i can get

--- lib/2023/day11.ex
+++ lib/2023/day11.ex
@@ -23,8 +23,8 @@ defmodule Aoc.Y2023.D11 do
       end)
     end)
     |> Task.async_stream(fn [{x1, y1}, {x2, y2}] ->
-      dist_x = Range.size(x1..x2) + Enum.count(x_double, &(&1 in x1..x2)) * (empty_len - 1) - 1
-      dist_y = Range.size(y1..y2) + Enum.count(y_double, &(&1 in y1..y2)) * (empty_len - 1) - 1
+      dist_x = manhattan_dist(x1, x2) + Enum.count(x_double, &(&1 in x1..x2)) * (empty_len - 1)
+      dist_y = manhattan_dist(y1, y2) + Enum.count(y_double, &(&1 in y1..y2)) * (empty_len - 1)
 
       dist_x + dist_y
     end)
@@ -35,9 +35,27 @@ defmodule Aoc.Y2023.D11 do
     part1(input, empty_len)
   end
 
+  def manhattan_dist(a, b) when a < b, do: b - a
+  def manhattan_dist(a, b) when a > b, do: a - b
+  def manhattan_dist(_, _), do: 0
+
   def helper(input) do
-    galaxies = :ets.new(:galaxies, [:public, write_concurrency: true, read_concurrency: true])
-    doubles = :ets.new(:doubles, [:public, :duplicate_bag])
+    galaxies =
+      :ets.new(:galaxies, [
+        :public,
+        write_concurrency: true,
+        read_concurrency: true,
+        decentralized_counters: true
+      ])
+
+    doubles =
+      :ets.new(:doubles, [
+        :public,
+        :duplicate_bag,
+        write_concurrency: true,
+        read_concurrency: true,
+        decentralized_counters: true
+      ])
 
     lines = input |> parse_lines()
     max_len = lines |> length()
@@ -54,20 +72,19 @@ defmodule Aoc.Y2023.D11 do
     lines
     |> Stream.map(&String.to_charlist/1)
     |> Stream.with_index()
-    |> Task.async_stream(fn {line, x} ->
-      line
-      |> Stream.with_index()
-      |> Enum.each(fn
-        {?#, y} ->
-          n = get_no.(1)
-          :ets.insert(galaxies, {{x, y}, n})
-          :ets.match_delete(doubles, {:x, x})
-          :ets.match_delete(doubles, {:y, y})
-          n
+    |> Stream.flat_map(fn {self, x} ->
+      Enum.with_index(self, fn char, y -> {char, x, y} end)
+    end)
+    |> Stream.dedup_by(fn {char, x, _} -> {char, x} end)
+    |> Task.async_stream(fn
+      {?#, x, y} ->
+        n = get_no.(1)
+        :ets.insert(galaxies, {{x, y}, n})
+        :ets.match_delete(doubles, {:x, x})
+        :ets.match_delete(doubles, {:y, y})
 
-        _ ->
-          get_no.(0)
-      end)
+      _ ->
+        nil
     end)
     |> Stream.run()
 
