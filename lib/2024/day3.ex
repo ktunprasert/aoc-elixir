7a8ab8384be2707b36128e13f2ee50b98d127fce
ktunprasert <ktunprasert@outlook.com>
2024-12-03 14:35:18 +0000

    2024.3: cooked part 2

--- lib/2024/day3.ex
+++ lib/2024/day3.ex
@@ -8,19 +8,43 @@ defmodule Aoc.Y2024.D3 do
   end
 
   def part2(input) do
-    :ok
+    solve(input, :part2)
   end
 
   def solve(input, part) do
-    re = ~r/mul\((\d{1,3}),(\d{1,3})\)/
-
     case part do
       :part1 ->
-        Regex.scan(re, input)
+        ~r/mul\((\d{1,3}),(\d{1,3})\)/
+        |> Regex.scan(input)
+        |> Enum.map(&tl/1)
+        |> Enum.map(fn lst -> lst |> Enum.map(&String.to_integer/1) |> Enum.product() end)
+        |> Enum.sum()
+
+      :part2 ->
+        ~r/(?:mul\((\d{1,3}),(\d{1,3})\))|(?:do(?:n't)?\(\))/
+        |> Regex.scan(input)
+        |> (&[["do()"] | &1]).()
+        |> find_usable()
         |> Enum.map(&tl/1)
         |> Enum.map(fn lst -> lst |> Enum.map(&String.to_integer/1) |> Enum.product() end)
         |> Enum.sum()
     end
+  end
+
+  def find_usable(lst), do: find_usable(lst, [])
+  def find_usable([], acc), do: acc
+
+  def find_usable([["do()"] | rest], acc) do
+    find_usable(rest, acc)
+  end
+
+  def find_usable([["don't()"] | rest], acc) do
+    Enum.drop_while(rest, &(&1 != ["do()"]))
+    |> find_usable(acc)
+  end
+
+  def find_usable([a | rest], acc) do
+    find_usable(rest, [a | acc])
   end
 
   def helper(input) do
